cec5b52d6a944c72a493bd64d8020d9368e1c4c0
numa08 <n511287@gmail.com>
2016-08-03 22:30:39 +0900

    Robolectric を利用する必要のある部分のテストを書きました
    Android のフレームワークに依存する部分について jvm でテストを行うときには Robolectric を使います。
    Robolectric は jvm 上で Android のフレームワークのコードの振る舞いを再現し、テストコードからアプリのコードが
    呼ばれたときの動作を確認できます。
    ただし、 Robolectric は内部で行うことがトリッキーであったり、フレームワークの振る舞いをテスト用にある程度実装したに
    過ぎない物なので、完全にテストを行うことはできないこともあります。
    よく見極めて、必要に応じて実機やエミュレータでのテストを採用することも必要です。

---
+++ app/src/test/java/net/numa08/android_testing_bootcamp3/ui/viewmodels/EventRowViewModelTest.java
@@ -0,0 +1,83 @@
+package net.numa08.android_testing_bootcamp3.ui.viewmodels;
+
+import android.view.LayoutInflater;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import net.numa08.android_testing_bootcamp3.databinding.ViewEventRowBinding;
+import net.numa08.android_testing_bootcamp3.models.Event;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+
+import static net.numa08.android_testing_bootcamp3.testtools.TestUtils.loadJson;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+// EventRowViewModel にデータをセットしたときに
+// データが正しく View に反映されることをテストします
+@RunWith(RobolectricTestRunner.class)
+public class EventRowViewModelTest {
+
+    // EventRowViewModel がセットされた Event から正しく
+    // View に描画するためのデータへ変換、抽出できていることを
+    // 確認します。
+    @Test
+    public void get_events_in_japan() throws Throwable {
+        final Event event = createEvent();
+        final ViewEventRowBinding binding = createViewEventRowBinding();
+        final EventRowViewModel viewModel = new EventRowViewModel(binding);
+
+        viewModel.setEvent(event);
+
+        // ViewModel から得られる値についてテストを行います
+        assertThat(viewModel.getMonth(), is("08"));
+        assertThat(viewModel.getDate(), is("05"));
+        // Robolectric がローカライズされた曜日を定義していないようなので
+        // テストができない
+//        assertThat(viewModel.getDateOfWeek(), is("金"));
+        assertThat(viewModel.getPlace(), is("東京都渋谷区神泉町8-16　渋谷ファーストプレイス8F"));
+        assertThat(viewModel.getTitle(), is("Android Testing Bootcamp #3"));
+        assertThat(viewModel.getStartedAt(), is("19:00"));
+    }
+
+    @Test
+    public void bind_model_to_view() throws Throwable {
+        final Event event = createEvent();
+        final ViewEventRowBinding binding = createViewEventRowBinding();
+        final EventRowViewModel viewModel = new EventRowViewModel(binding);
+
+        viewModel.setEvent(event);
+
+        // ViewModel に値をセットしたあと、バインドされている View について描画が更新されていることを
+        // テストします
+        assertThat(binding.monthText.getText().toString(), is("08"));
+        assertThat(binding.dateText.getText().toString(), is("05"));
+        // Robolectric がローカライズされた曜日を定義していないようなので
+        // テストができない
+//        assertThat(binding.dateOfWeekText.getText().toString(), is("金"));
+        assertThat(binding.placeText.getText().toString(), is("東京都渋谷区神泉町8-16　渋谷ファーストプレイス8F"));
+        assertThat(binding.titleText.getText().toString(), is("Android Testing Bootcamp #3"));
+        assertThat(binding.startedAtText.getText().toString(), is("19:00"));
+    }
+
+    private ViewEventRowBinding createViewEventRowBinding() {
+        final LayoutInflater layoutInflater = LayoutInflater.from(RuntimeEnvironment.application);
+        return ViewEventRowBinding.inflate(layoutInflater, null, false);
+    }
+
+    private Event createEvent() throws Throwable {
+        final String json = loadJson("event.json");
+        final Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
+                .create();
+        return gson.fromJson(json, Event.class);
+    }
+
+
+}
